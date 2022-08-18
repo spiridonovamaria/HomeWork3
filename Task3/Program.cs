40a47d095c28658f7242bd96986891306368bc9d
Maria Spiridonova <traffic2007@mail.ru>
2022-08-18 21:54:39 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿int Prompt(string message)
+{
+System.Console.Write(message); // Вывести сообщение
+string readValue = Console.ReadLine(); // Считавает с консоли строку
+int result = int.Parse(readValue); // Преобрадует строку в целое число
+return result; // Возвращает результат
+}
+
+// Напишите программу, которая принимает на вход число (N)
+//и выдаёт таблицу кубов чисел от 1 до N.
+
+
+void takeN(int N)
+{
+for (int i = 1; i <= N; i++)
+{
+System.Console.Write(i * i * i + " ");
+}
+}
+
+takeN(Prompt("Введите число> "));
